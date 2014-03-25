9e34530cb6c4807cf3be0c750a95b59ec58a1fa7
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-03-25 16:35:25 +0100

    //new version of module gamification fixed bug #PSCSX-1368

--- controllers/admin/AdminGamificationController.php
+++ controllers/admin/AdminGamificationController.php
@@ -295,7 +295,7 @@ class AdminGamificationController extends ModuleAdminController
 	public function ajaxProcessCloseAdvice()
 	{
 		$id_advice = Advice::getIdByIdPs((int)Tools::getValue('id_advice'));
-		Db::getInstance()->execute('UPDATE `ps_advice` SET `hide` =  \'1\' WHERE  `id_advice` = '.(int)$id_advice.';');
+		Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'advice` SET `hide` =  \'1\' WHERE  `id_advice` = '.(int)$id_advice.';');
 		die();
 	}
 }
