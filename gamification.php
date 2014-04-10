8bb06f6ef37052521b91a8ec1f4eaddeb81cb6f4
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-04-10 17:52:04 +0200

    //new version gamification

--- gamification.php
+++ gamification.php
@@ -38,7 +38,7 @@ class Gamification extends Module
 	{
 		$this->name = 'gamification';
 		$this->tab = 'administration';
-		$this->version = '1.8.7';
+		$this->version = '1.8.8';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
