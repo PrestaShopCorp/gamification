aa617fc11ee714026ffdec2cd924c5cd84cff873
Thibaud Chauviere <thibaud.chauviere@gmail.com>
2014-07-02 11:12:58 +0200

    // Version changed

--- gamification.php
+++ gamification.php
@@ -38,7 +38,7 @@ class Gamification extends Module
 	{
 		$this->name = 'gamification';
 		$this->tab = 'administration';
-		$this->version = '1.9.2';
+		$this->version = '1.9.3';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
