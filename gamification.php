963145f90bad69a7c358dbd7159028d4a174eb49
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-08-23 11:06:56 +0200

    // Version updated to v1.9.8

--- gamification.php
+++ gamification.php
@@ -38,7 +38,7 @@ class Gamification extends Module
 	{
 		$this->name = 'gamification';
 		$this->tab = 'administration';
-		$this->version = '1.9.7';
+		$this->version = '1.9.8';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
