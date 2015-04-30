b6087b9626c28711dbba3d799c9b512bbf01cc38
joce <jocelyn.Fournier@gmail.com>
2015-04-30 15:48:16 +0200

    [*] Defered call to speed up import

--- gamification.php
+++ gamification.php
@@ -128,26 +128,26 @@ class Gamification extends Module
 
 	public function __call($name, $arguments)
 	{
-		if (!Validate::isHookName($name))
-			return false;
-		$name = str_replace('hook', '', $name);
-
-
-		if (!Cache::isStored($name))
+		if (!empty(self::$_in_import))
 		{
-			if ($retro_name = Db::getInstance()->getValue('SELECT `name` FROM `'._DB_PREFIX_.'hook_alias` WHERE `alias` = \''.pSQL($name).'\''))
-				$name = $retro_name;
-
-			$condition_ids = Condition::getIdsByHookCalculation($name);
-			Cache::store($name, $condition_ids);
+			self::$_defered_func_call[get_class().'::__call_'.$name] = array(array($this, '__call'), array($name, $arguments));
 		}
 		else
-			$condition_ids = Cache::retrieve($name);
-
-		foreach ($condition_ids as $id)
 		{
-			$cond = new Condition((int)$id);
-			$cond->processCalculation();
+			if (!Validate::isHookName($name)) {
+				return false;
+			}
+			$name = str_replace('hook', '', $name);
+
+			if ($retro_name = Db::getInstance()->getValue('SELECT `name` FROM `'._DB_PREFIX_.'hook_alias` WHERE `alias` = \''.pSQL($name).'\'')) {
+				$name = $retro_name;
+			}
+
+			$condition_ids = Condition::getIdsByHookCalculation($name);
+			foreach ($condition_ids as $id) {
+				$cond = new Condition((int)$id);
+				$cond->processCalculation();
+			}
 		}
 	}
 
