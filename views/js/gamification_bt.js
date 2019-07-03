ca71afa1f01bbbeb5fe3c0cf40b941a7e981123e
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-07-03 15:24:00 +0200

    Do not trigger if variable is not defined

--- views/js/gamification_bt.js
+++ views/js/gamification_bt.js
@@ -1,5 +1,7 @@
 $(document).ready( function () {
-  gamificationTasks();
+  if (typeof admin_gamification_ajax_url !== 'undefined') {
+    gamificationTasks();
+  }
 });
 
 function gamificationTasks()
